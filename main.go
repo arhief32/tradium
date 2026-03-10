968ff679c66c31bc83fcc9c2ad81c7e782859820
Arief Muarrief <arhief32@gmail.com>
2026-03-10 12:36:30 +0700

    init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	for {
+		log.Println("Checking market...")
+
+		// ambil data market
+		// hitung indikator
+		// kirim signal
+
+		time.Sleep(30 * time.Second)
+	}
+}
